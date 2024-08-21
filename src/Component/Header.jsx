24bd01a7844f42b766ff6bf713a3309042512404
sohil khan <skb90011@gmail.com>
2024-08-21 18:01:49 +0530

    commit

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -7,15 +7,15 @@ const Header = () => {
       <div className="flex items-center justify-between flex-wrap sm:flex-nowrap">
         <div className="flex items-center">
           <a href="/" className="flex-none min-w-[80px] sm:min-w-[100px]">
-            <div className="relative pl-2 w-full h-16 sm:h-20">
+            <div className="relative pl-1 w-full h-16 sm:h-20">
               <img
                 alt="logo"
                 src="https://i.imgur.com/Z5Hf8eH.png"
-                className="object-contain h-full pl-4 sm:pl-10"
+                className="object-contain h-full"
               />
             </div>
           </a>
-          <div className="ml-2 sm:ml-4 hidden sm:block">
+          <div className="ml-4 sm:ml-6 hidden sm:block">
             <h1 className="text-xl sm:text-2xl font-bold">
               KISAN Agriculture Workshop
             </h1>
@@ -37,7 +37,7 @@ const Header = () => {
             className="group flex items-center disabled:opacity-50 disabled:hover:opacity-50 hover:opacity-95 justify-center ring-none rounded-lg shadow-lg font-semibold 
   py-1 px-3 text-sm sm:py-2 sm:px-4 sm:text-base font-dm focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-gray-100 border-b-4 border-b-green-600 disabled:border-0
   disabled:bg-white disabled:text-gray-800 ring-white text-gray-800 hover:border-0 active:border-0 hover:text-gray-800 active:bg-gray-200
-  active:text-gray-800 focus-visible:outline-green-500 dark:bg-gray-100 dark:border-gray-700 dark:border-b-green-600 mr-4 sm:mr-10"
+  active:text-gray-800 focus-visible:outline-green-500 dark:bg-gray-100 dark:border-gray-700 dark:border-b-green-600 mr-2 sm:mr-10"
             href="tel:9828719163"
           >
             <FaPhoneAlt className="mr-1 sm:mr-2 text-black" />

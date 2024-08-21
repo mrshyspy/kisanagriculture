f42032786b18c4761d292d351289d6a9cb7e9b26
sohil khan <skb90011@gmail.com>
2024-08-21 17:38:12 +0530

    commit

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -3,11 +3,11 @@ import { FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className="bg-white shadow-lg p-2 sm:p-0">
+    <header className="bg-white shadow-lg p-0 sm:p-0">
       <div className="flex items-center justify-between flex-wrap sm:flex-nowrap">
         <div className="flex items-center">
           <a href="/" className="flex-none min-w-[80px] sm:min-w-[100px]">
-            <div className="relative w-full h-16 sm:h-20">
+            <div className="relative pl-2 w-full h-16 sm:h-20">
               <img
                 alt="logo"
                 src="https://i.imgur.com/Z5Hf8eH.png"
@@ -34,17 +34,20 @@ const Header = () => {
         </div>
         <div className="flex items-center mt-2 sm:mt-0">
           <a
-            className="group flex items-center justify-center ring-none rounded-lg shadow-lg font-semibold 
-            py-1 px-3 sm:py-2 sm:px-4 text-xs sm:text-sm bg-gray-100 border-b-4 border-b-green-600 mr-6 sm:mr-10"
+            className="group flex h-min items-center disabled:opacity-50 disabled:hover:opacity-50 hover:opacity-95 justify-center ring-none rounded-lg shadow-lg font-semibold 
+  py-2 px-4 font-dm focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-gray-100 border-b-4 border-b-green-600 disabled:border-0
+  disabled:bg-white disabled:text-gray-800 ring-white text-gray-800 hover:border-0 active:border-0 hover:text-gray-800 active:bg-gray-200
+  active:text-gray-800 focus-visible:outline-green-500 text-sm sm:text-base dark:bg-gray-100 dark:border-gray-700 dark:border-b-green-600 mr-10"
             href="tel:9828719163"
           >
-            <FaPhoneAlt className="mr-1 sm:mr-2 text-black" />
+            <FaPhoneAlt className="mr-2 text-black" />
             +91-9828 7191 63
           </a>
         </div>
       </div>
+
       <div className="w-full bg-gray-100">
-        <nav className="py-1 sm:py-2">
+        <nav className="py-1 ml-0 mr-0 sm:py-2">
           <ul className="flex gap-4 sm:gap-6 justify-center text-black uppercase text-xs sm:text-sm font-medium">
             <li>
               <a

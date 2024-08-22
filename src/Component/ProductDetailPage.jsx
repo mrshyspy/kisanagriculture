5df6778448b5e9d63e6094ca96b4c36b17979ed0
sohil khan <skb90011@gmail.com>
2024-08-22 10:12:21 +0530

    ..

--- src/Component/ProductDetailPage.jsx
+++ src/Component/ProductDetailPage.jsx
@@ -1,25 +1,22 @@
-// TechnicalSpecificationsPage.jsx
 import React from 'react';
 import QuickLinks from './QuickLink';
-import Breadcrumbs from './Breadcrumb'; // Import the Breadcrumbs component
+import Breadcrumbs from './Breadcrumb';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-
 function ProductDetailPage({ threshers }) {
-    const images = [
-        'https://i.imgur.com/nkjprq9.png',
-        'https://i.imgur.com/IEAum1D.jpeg',
-        'https://i.imgur.com/nkjprq9.png',
-        'https://i.imgur.com/IEAum1D.jpeg',
-      ];
-    const { ProductId } = useParams();
-    const [selectedImage, setSelectedImage] = useState(images[0]);
-    const [isZoomed, setIsZoomed] = useState(false);
+  const images = [
+    'https://i.imgur.com/nkjprq9.png',
+    'https://i.imgur.com/IEAum1D.jpeg',
+    'https://i.imgur.com/nkjprq9.png',
+    'https://i.imgur.com/IEAum1D.jpeg',
+  ];
+  const { ProductId } = useParams();
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+  const [isZoomed, setIsZoomed] = useState(false);
 
-    const thresher = threshers.find(t => t.productId === ProductId);
+  const thresher = threshers.find(t => t.productId === ProductId);
 
-  
   const specs = [
     { label: 'Gear Box', value: '21-21 Heavy Duty' },
     { label: 'Drum Size', value: '45 Inch / 49 Inch' },
@@ -42,111 +39,107 @@ function ProductDetailPage({ threshers }) {
     'Superior Chain Drives.',
   ];
 
-  // Define breadcrumb paths
   const breadcrumbPaths = [
     { label: 'Home', href: '/' },
     { label: thresher ? thresher.ModelName : 'Product', href: `/${ProductId}` },
   ];
 
- 
-
   if (!thresher) {
     return <div>Product not found</div>;
   }
+
   return (
     <>
       <div className="container mx-auto p-6 bg-white text-gray-800">
-        <Breadcrumbs paths={breadcrumbPaths} /> {/* Add Breadcrumbs here */}
+        <Breadcrumbs paths={breadcrumbPaths} />
         <div className="flex flex-col lg:flex-row lg:space-x-12 bg-gray-50 p-4 md:p-6 shadow-md">
-      {/* Left side: Carousel */}
-      <div className="lg:w-1/2">
-        <div
-          className="relative overflow-hidden rounded-xl shadow-lg"
-          onMouseEnter={() => setIsZoomed(true)}
-          onMouseLeave={() => setIsZoomed(false)}
-        >
-          <img
-            src={selectedImage}
-            alt="Selected"
-            className={`object-cover w-full h-56 md:h-80 transition-transform duration-500 ${
-              isZoomed ? 'scale-110' : 'scale-100'
-            }`}
-          />
-          {/* Modernized Navigation arrows */}
-          <button
-            className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white bg-opacity-60 text-green-600 p-3 rounded-full shadow-lg hover:bg-opacity-100 hover:text-white hover:bg-gradient-to-r from-green-600 to-green-400 transition-all duration-300"
-            onClick={() => {
-              const currentIndex = images.indexOf(selectedImage);
-              const nextIndex = (currentIndex - 1 + images.length) % images.length;
-              setSelectedImage(images[nextIndex]);
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              className="w-5 h-5"
+          {/* Left side: Carousel */}
+          <div className="lg:w-1/2">
+            <div
+              className="relative overflow-hidden rounded-xl shadow-lg"
+              onMouseEnter={() => setIsZoomed(true)}
+              onMouseLeave={() => setIsZoomed(false)}
             >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
-            </svg>
-          </button>
-          <button
-            className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white bg-opacity-60 text-green-600 p-3 rounded-full shadow-lg hover:bg-opacity-100 hover:text-white hover:bg-gradient-to-r from-green-600 to-green-400 transition-all duration-300"
-            onClick={() => {
-              const currentIndex = images.indexOf(selectedImage);
-              const nextIndex = (currentIndex + 1) % images.length;
-              setSelectedImage(images[nextIndex]);
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              className="w-5 h-5"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
-            </svg>
-          </button>
-        </div>
-        <div className="flex space-x-3 mt-4 md:mt-6 justify-center">
-          {images.map((img, index) => (
-            <img
-              key={index}
-              src={img}
-              alt={`Thumbnail ${index + 1}`}
-              className={`w-16 h-16 md:w-20 md:h-20 cursor-pointer rounded-lg border-b-4 ${
-                selectedImage === img ? 'border-b-green-600' : 'border-b-gray-300'
-              } shadow-sm transform transition-transform duration-300 hover:scale-110`}
-              onClick={() => setSelectedImage(img)}
-            />
-          ))}
-        </div>
-      </div>
+              <img
+                src={selectedImage}
+                alt="Selected"
+                className={`object-cover w-full h-48 sm:h-56 md:h-64 lg:h-72 xl:h-80 transition-transform duration-500 ${
+                  isZoomed ? 'scale-110' : 'scale-100'
+                }`}
+              />
+              {/* Modernized Navigation arrows */}
+              <button
+                className="absolute top-1/2 left-2 sm:left-4 transform -translate-y-1/2 bg-white bg-opacity-60 text-green-600 p-2 sm:p-3 rounded-full shadow-lg hover:bg-opacity-100 hover:text-white hover:bg-gradient-to-r from-green-600 to-green-400 transition-all duration-300"
+                onClick={() => {
+                  const currentIndex = images.indexOf(selectedImage);
+                  const nextIndex = (currentIndex - 1 + images.length) % images.length;
+                  setSelectedImage(images[nextIndex]);
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="currentColor"
+                  className="w-4 h-4 sm:w-5 sm:h-5"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+                </svg>
+              </button>
+              <button
+                className="absolute top-1/2 right-2 sm:right-4 transform -translate-y-1/2 bg-white bg-opacity-60 text-green-600 p-2 sm:p-3 rounded-full shadow-lg hover:bg-opacity-100 hover:text-white hover:bg-gradient-to-r from-green-600 to-green-400 transition-all duration-300"
+                onClick={() => {
+                  const currentIndex = images.indexOf(selectedImage);
+                  const nextIndex = (currentIndex + 1) % images.length;
+                  setSelectedImage(images[nextIndex]);
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="currentColor"
+                  className="w-4 h-4 sm:w-5 sm:h-5"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+                </svg>
+              </button>
+            </div>
+            <div className="flex space-x-3 mt-4 md:mt-6 justify-center">
+              {images.map((img, index) => (
+                <img
+                  key={index}
+                  src={img}
+                  alt={`Thumbnail ${index + 1}`}
+                  className={`w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-20 lg:h-20 cursor-pointer rounded-lg border-b-4 ${
+                    selectedImage === img ? 'border-b-green-600' : 'border-b-gray-300'
+                  } shadow-sm transform transition-transform duration-300 hover:scale-110`}
+                  onClick={() => setSelectedImage(img)}
+                />
+              ))}
+            </div>
+          </div>
 
-      {/* Right side: Product description */}
-      <div className="lg:w-1/2 flex flex-col justify-center space-y-4 md:space-y-6 mt-4 lg:mt-0">
-        <h1 className="text-2xl md:text-3xl font-bold text-gray-800">{thresher.ModelName}</h1>
-        <p className="text-gray-700 leading-relaxed">
-        {thresher.details.description}
-        </p>
-        <div className="flex flex-col sm:flex-row space-y-3 sm:space-y-0 sm:space-x-4">
-          <button className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 md:px-6 md:py-3 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105">
-            Get On Road Price
-          </button>
-          <button className="bg-gray-200 hover:bg-gray-300 text-gray-700 px-4 py-2 md:px-6 md:py-3 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105">
-            Download Brochure
-          </button>
+          {/* Right side: Product description */}
+          <div className="lg:w-1/2 flex flex-col justify-center space-y-4 md:space-y-6 mt-4 lg:mt-0">
+            <h1 className="text-2xl md:text-3xl font-bold text-gray-800">{thresher.ModelName}</h1>
+            <p className="text-gray-700 leading-relaxed">{thresher.details.description}</p>
+            <div className="flex flex-col sm:flex-row space-y-3 sm:space-y-0 sm:space-x-4">
+              <button className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 md:px-6 md:py-3 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105">
+                Get On Road Price
+              </button>
+              <button className="bg-gray-200 hover:bg-gray-300 text-gray-700 px-4 py-2 md:px-6 md:py-3 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105">
+                Download Brochure
+              </button>
+            </div>
+            <p className="text-xs text-gray-400 mt-4">
+              Disclaimer: These photographs are for illustrative purposes only and may be subject to change.
+            </p>
+          </div>
         </div>
-        <p className="text-xs text-gray-400 mt-4">
-          Disclaimer: These photographs are for illustrative purposes only and may be subject to change.
-        </p>
-      </div>
-    </div>
-        <h2 className=" mt-8 text-2xl font-semibold mb-4">Technical Specifications</h2>
+        <h2 className="mt-8 text-2xl font-semibold mb-4">Technical Specifications</h2>
         <div className="overflow-x-auto mb-12">
           <table className="min-w-full bg-white text-gray-800 shadow-lg rounded-lg border border-gray-300">
             <tbody>
@@ -167,24 +160,23 @@ function ProductDetailPage({ threshers }) {
           <h3 className="text-2xl font-semibold mb-4">Features</h3>
           <ul className="list-disc list-inside">
             {features.map((feature, index) => (
-              <li key={index} className="mb-2">{feature}</li>
+              <li key={index} className="mb-2">
+                {feature}
+              </li>
             ))}
           </ul>
         </div>
-        <div className='pt-1 pb-1'>
-          <QuickLinks/>
+        <div className="pt-1 pb-1">
+          <QuickLinks />
         </div>
 
         <div className="bg-white p-6 shadow-lg">
           <h3 className="text-2xl font-semibold mb-4">Watch Videos</h3>
-          <div className="relative w-full overflow-hidden pb-[56.25%] h-0">
+          <div className="relative w-full overflow-hidden pb-[56.25%] h-0 rounded-xl shadow-lg">
             <iframe
               className="absolute top-0 left-0 w-full h-full"
-              src="https://www.youtube.com/embed/4n32giNG1rw?si=mFkbgvKpSr52oVWE"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
+              src="https://www.youtube.com/embed/QK9hM4wU-eU"
+              title="YouTube video"
               allowFullScreen
             ></iframe>
           </div>

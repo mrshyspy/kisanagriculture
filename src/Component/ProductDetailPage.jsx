9a712969e07f8ff5380794969564becf68588530
sohil khan <skb90011@gmail.com>
2024-08-22 10:21:04 +0530

    ..

--- src/Component/ProductDetailPage.jsx
+++ src/Component/ProductDetailPage.jsx
@@ -50,11 +50,11 @@ function ProductDetailPage({ threshers }) {
 
   return (
     <>
-      <div className="container mx-auto p-6 bg-white text-gray-800">
+      <div className="container mx-auto p-4 md:p-6 bg-white text-gray-800">
         <Breadcrumbs paths={breadcrumbPaths} />
         <div className="flex flex-col lg:flex-row lg:space-x-12 bg-gray-50 p-4 md:p-6 shadow-md">
           {/* Left side: Carousel */}
-          <div className="lg:w-1/2">
+          <div className="lg:w-1/2 mb-4 lg:mb-0">
             <div
               className="relative overflow-hidden rounded-xl shadow-lg"
               onMouseEnter={() => setIsZoomed(true)}
@@ -67,7 +67,7 @@ function ProductDetailPage({ threshers }) {
                   isZoomed ? 'scale-110' : 'scale-100'
                 }`}
               />
-              {/* Modernized Navigation arrows */}
+              {/* Navigation arrows */}
               <button
                 className="absolute top-1/2 left-2 sm:left-4 transform -translate-y-1/2 bg-white bg-opacity-60 text-green-600 p-2 sm:p-3 rounded-full shadow-lg hover:bg-opacity-100 hover:text-white hover:bg-gradient-to-r from-green-600 to-green-400 transition-all duration-300"
                 onClick={() => {
@@ -107,7 +107,8 @@ function ProductDetailPage({ threshers }) {
                 </svg>
               </button>
             </div>
-            <div className="flex space-x-3 mt-4 md:mt-6 justify-center">
+            {/* Image thumbnails */}
+            <div className="flex space-x-2 mt-4 justify-center">
               {images.map((img, index) => (
                 <img
                   key={index}
@@ -123,9 +124,13 @@ function ProductDetailPage({ threshers }) {
           </div>
 
           {/* Right side: Product description */}
-          <div className="lg:w-1/2 flex flex-col justify-center space-y-4 md:space-y-6 mt-4 lg:mt-0">
-            <h1 className="text-2xl md:text-3xl font-bold text-gray-800">{thresher.ModelName}</h1>
-            <p className="text-gray-700 leading-relaxed">{thresher.details.description}</p>
+          <div className="lg:w-1/2 flex flex-col justify-center space-y-4 md:space-y-6">
+            <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-800">
+              {thresher.ModelName}
+            </h1>
+            <p className="text-gray-700 leading-relaxed">
+              {thresher.details.description}
+            </p>
             <div className="flex flex-col sm:flex-row space-y-3 sm:space-y-0 sm:space-x-4">
               <button className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 md:px-6 md:py-3 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105">
                 Get On Road Price

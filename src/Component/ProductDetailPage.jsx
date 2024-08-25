8cda2701cf9f670f688782a8968b3274c05af1d1
sohil khan <skb90011@gmail.com>
2024-08-25 19:21:20 +0530

    ss

--- src/Component/ProductDetailPage.jsx
+++ src/Component/ProductDetailPage.jsx
@@ -172,7 +172,7 @@ function ProductDetailPage({ threshers }) {
             ))}
           </ul>
         </div>
-        <div className="pt-1 pb-1">
+        <div className=" px-0 mx-0 pt-1 pb-1">
           <QuickLinks />
         </div>
 

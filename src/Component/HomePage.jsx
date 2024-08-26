29d7f4b2e14730b01f93be7f2cf648e1f285c47b
sohil khan <skb90011@gmail.com>
2024-08-26 18:30:15 +0530

    ss

--- src/Component/HomePage.jsx
+++ src/Component/HomePage.jsx
@@ -16,7 +16,7 @@ const HomePage = () => {
           style={{ zIndex: -1 }}
         ></div>
 
-        <div className="container mx-auto p-6 bg-gray-100 rounded-lg rounded-b-none shadow-md">
+        <div className="container mx-auto p-6 bg-gradient-to-tr from-yellow-100 to-green-100 rounded-lg rounded-b-none shadow-md">
           <div className="text-center mb-12 p-2">
             <h1 className="text-3xl sm:text-3xl md:text-4xl font-extrabold text-gray-800 mb-4 mt-10">
               Discover Our Wide Range of{" "}

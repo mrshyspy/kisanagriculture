bab4585e3226c9a01c1f6b5ea9eda3f6e0aee6f4
sohil khan <skb90011@gmail.com>
2024-08-29 18:08:12 +0530

    ss

--- src/Component/Banners.jsx
+++ src/Component/Banners.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Banners() {
   const bannerData = [
     {
-      title: "High Performance",
+      title: "Heavy Duty",
       subtitle: "Maximize your efficiency with our high-performance threshers.",
       bgClass: "bg-gradient-to-br from-green-600 to-green-500" // Custom gradient color
     },
@@ -18,12 +18,12 @@ function Banners() {
       bgClass: "bg-gradient-to-br from-yellow-600 to-yellow-500" // Custom gradient color
     },
     {
-      title: "Ease of Use",
+      title: "Easy to Use",
       subtitle: "User-friendly machines for effortless operation.",
       bgClass: "bg-gradient-to-br from-purple-600 to-purple-500" // Custom gradient color
     },
     {
-      title: "Built to last   ",
+      title: "Built to last",
       subtitle: "Built to last with exceptional performance for every harvest.",
       bgClass: "bg-gradient-to-br from-teal-600 to-teal-500" // Custom gradient color
     },
@@ -47,7 +47,7 @@ function Banners() {
             key={index}
             className={`flex flex-col justify-center items-center ${banner.bgClass} py-4 sm:py-6 px-4 sm:px-6 rounded-lg shadow-lg space-y-3 sm:space-y-4`}
           >
-            <h1 className="text-lg sm:text-lg md:text-xl font-bold text-white text-center">
+            <h1 className="text-lg sm:text-lg md:text-2xl font-bold text-white text-center">
               {banner.title}
             </h1>
             {/* <p className="text-sm sm:text-base md:text-lg text-white text-center">

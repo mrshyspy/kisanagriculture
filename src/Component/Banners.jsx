a7150dfcd991a091d1b344c87a6d8bc0b47a95f9
sohil khan <skb90011@gmail.com>
2024-08-29 13:09:22 +0530

    ss

--- src/Component/Banners.jsx
+++ src/Component/Banners.jsx
@@ -41,7 +41,7 @@ function Banners() {
           Quality Before Quantity
         </h2>
       </div>
-      <div className="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {bannerData.map((banner, index) => (
           <div
             key={index}

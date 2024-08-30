2c4d22b30c8e18bf8b8803c93afeb689846c883f
sohil khan <skb90011@gmail.com>
2024-08-30 13:20:01 +0530

    ss

--- src/Component/HomePage.jsx
+++ src/Component/HomePage.jsx
@@ -20,7 +20,7 @@ const HomePage = () => {
 
         <div className="container mx-auto p-6 bg-gradient-to-tr from-yellow-100 to-green-100 rounded-lg rounded-b-none shadow-md">
           <div className="text-center mb-12 p-2">
-          <p className=" text-base sm:text-lg md:text-xl font-medium text-gray-800 mt-8">Serving Thousands Of Happy Customer{" "}<span className="text-red-600"> Since 1998</span> </p>
+          <p className=" text-base sm:text-lg md:text-xl font-medium text-gray-800 mt-8">Serving Thousands Of Happy Customer{" "}<span className="text-red-600 font-semibold "> Since 1998</span> </p>
 
             <h1 className="text-3xl sm:text-3xl md:text-4xl font-extrabold text-gray-800 mb-12 mt-1">
               Discover Our Wide Range of{" "}
@@ -28,7 +28,7 @@ const HomePage = () => {
             </h1>
 
             <div>
-            <p className="border-b-green-600 border-b-4 text-base text-justify sm:text-lg md:text-xl leading-relaxed text-gray-700 bg-green-50 p-6 rounded-lg shadow-md max-w-3xl mx-auto sm:mx-4 md:mx-auto">
+            <p className="border-b-green-600 border-b-4 text-base text-balance sm:text-lg md:text-xl leading-relaxed text-gray-700 bg-green-50 p-6 rounded-lg shadow-md max-w-3xl mx-auto sm:mx-4 md:mx-auto">
               We have designed thresher machines for different agricultural
               needs. Choose from single, double, and three fan models, with and
               without elevators. Each machine is tailored to provide efficiency

e3f857fc36822f263cbe8edd2b1979c83cddcc66
sohil khan <skb90011@gmail.com>
2024-09-04 20:06:27 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './Component/Header';
-import './index.css';
 import Footer from './Component/Footer';
 import HomePage from './Component/HomePage';
 import ProductDetail from './Component/ProductDetail';
@@ -8,8 +7,8 @@ import TechnicalSpecifications from './Component/Specs';
 import threshers from './Component/Threshers';
 import ProductDetailPage from './Component/ProductDetailPage';
 import ContactUs from './Component/ContactUs';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Gallery from './Component/Gallery';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -19,9 +18,9 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/product-detail" element={<ProductDetail />} />
         <Route path="/product/:ProductId" element={<TechnicalSpecifications threshers={threshers} />} />
-        <Route path="/:ProductId" element={<ProductDetailPage threshers={threshers} />} />
-        <Route path="/contact-us" element={<ContactUs/>} />
-        <Route path="/gallery" element = {<Gallery/>} />
+        <Route path="/details/:ProductId" element={<ProductDetailPage threshers={threshers} />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/gallery" element={<Gallery />} />
       </Routes>
       <Footer />
     </Router>

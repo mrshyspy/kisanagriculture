b3aa7277355c27370be813e8ed843d237ccae290
sohil khan <skb90011@gmail.com>
2024-09-04 20:07:15 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Component/Header';
+import './index.css';
 import Footer from './Component/Footer';
 import HomePage from './Component/HomePage';
 import ProductDetail from './Component/ProductDetail';
@@ -7,8 +8,8 @@ import TechnicalSpecifications from './Component/Specs';
 import threshers from './Component/Threshers';
 import ProductDetailPage from './Component/ProductDetailPage';
 import ContactUs from './Component/ContactUs';
-import Gallery from './Component/Gallery';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Gallery from './Component/Gallery';
 
 function App() {
   return (
@@ -18,9 +19,9 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/product-detail" element={<ProductDetail />} />
         <Route path="/product/:ProductId" element={<TechnicalSpecifications threshers={threshers} />} />
-        <Route path="/details/:ProductId" element={<ProductDetailPage threshers={threshers} />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/:ProductId" element={<ProductDetailPage threshers={threshers} />} />
+        <Route path="/contact-us" element={<ContactUs/>} />
+        <Route path="/gallery" element = {<Gallery/>} />
       </Routes>
       <Footer />
     </Router>

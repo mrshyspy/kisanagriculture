606608fe86bb6914c1d608b3fe89d7d16ca16f43
sohil khan <skb90011@gmail.com>
2024-09-11 17:33:21 +0530

    ss

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -5,8 +5,8 @@ function Banner() {
   return (
     <div className=" container mx-auto py-8 px-6 sm:px-8 lg:px-12">
       <div className="flex flex-col md:flex-row items-stretch justify-center space-y-4 md:space-y-0 md:space-x-6 lg:space-x-8">
-      <div className="flex flex-col md:flex-row items-center justify-between bg-yellow-400 dark:bg-gray-800 p-6 md:p-12 lg:px-12 md:w-8/12 lg:w-7/12 xl:w-8/12 2xl:w-9/12 rounded-lg shadow-lg">
-      <div className="flex flex-col justify-center md:w-1/2">
+        <div className="mb-4 flex flex-col md:flex-row items-center justify-between bg-yellow-400 dark:bg-gray-800 p-6 md:p-12 lg:px-12 md:w-8/12 lg:w-7/12 xl:w-8/12 2xl:w-9/12 rounded-lg shadow-lg">
+          <div className="flex flex-col justify-center md:w-1/2">
             <h1 className="text-3xl lg:text-5xl font-bold text-gray-800 dark:text-white">
               All <span className="text-green-600">INDIA</span> Delivery
             </h1>
--- src/Component/Banners.jsx
+++ src/Component/Banners.jsx
@@ -43,7 +43,7 @@ export default function Banners() {
   ];
 
   return (
-    <div className="container mx-auto py-8 px-6 sm:px-8 lg:px-12">
+    <div className="container mx-auto py-8 px-6 sm:px-8 lg:px-12 bg-gray-100 shadow-lg">
       <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-800 dark:text-gray-200 mb-12 animate-fade-in-down">
         Quality Before Quantity
       </h2>

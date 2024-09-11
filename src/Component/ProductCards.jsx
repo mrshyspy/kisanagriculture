d09d8ec951c4fed15e885b0c72dfaf24fcefffa3
sohil khan <skb90011@gmail.com>
2024-09-11 15:45:56 +0530

    ss

--- src/Component/ProductCards.jsx
+++ src/Component/ProductCards.jsx
@@ -43,7 +43,7 @@ const ProductCard = () => {
                 </h3>
               </div>
               <div className="p-4 sm:p-6">
-                <div className="space-y-2 text-s sm:text-sm text-gray-800">
+                <div className="space-y-2 text-sm sm:text-sm text-gray-800">
                   {Object.entries(machine.details.specification).map(
                     ([key, value]) => (
                       <div
